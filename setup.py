f134939c7dbef7b2f6f1e747a6c2a10c90f963f7
persianpros <persianpros@yahoo.com>
2017-12-20 14:27:28 +0330

    Follow e2openplugins structure

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+import setup_translate
+
+pkg = 'Extensions.BackupSuite'
+setup (name = 'enigma2-plugin-extensions-backupsuite',
+       version = '0.22',
+       description = 'Back-up tool for various enigma2 receivers',
+       packages = [pkg],
+       package_dir = {pkg: 'plugin'},
+       package_data = {pkg: ['*.sh', '*.txt', 'mphelp.xml', 'plugin.png', '*/*.png', 'locale/*/LC_MESSAGES/*.mo']},
+       cmdclass = setup_translate.cmdclass, # for translation
+      )
