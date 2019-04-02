dbd826bc5e3d318eb6df392c206b817831406795
Persian Prince <persianpros@gmail.com>
2019-04-02 09:06:30 +0430

    Update plugin.py

--- plugin/plugin.py
+++ plugin/plugin.py
@@ -384,7 +384,7 @@ class FlashImageConfig(Screen):
 						text += "kernel_cfe_auto.bin, root_cfe_auto.jffs2"
 
 				else:
-					if model in ("hd51","hd60","h7","sf4008","sf5008","sf8008","vs1500","et11000","et13000","bre2ze4k","spycat4k","spycat4kmini","protek4k","cc1") or model.startswith(("anadol","axashis4","dinobot4","ferguson4","mediabox4k","axashisc4")):
+					if model in ("hd51","hd60","h7","sf4008","sf5008","sf8008","vs1500","et11000","et13000","bre2ze4k","spycat4k","spycat4kmini","protek4k","cc1") or model.startswith(("anadol","axashis4","dinobot4","ferguson4","mediabox4","axashisc4")):
 						backup_files = [("kernel.bin"), ("rootfs.tar.bz2")]
 						no_backup_files = [("kernel_cfe_auto.bin"), ("rootfs.bin"), ("root_cfe_auto.jffs2"), ("root_cfe_auto.bin"), ("oe_kernel.bin"), ("oe_rootfs.bin"), ("kernel_auto.bin"), ("uImage"), ("rootfs.ubi")]
 						text += "kernel.bin, rootfs.tar.bz2"
