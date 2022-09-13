e5be5ee87ab3e0b418526beced63f26e36a373b1
AYP <persianpros@yahoo.com>
2022-09-13 15:51:41 +0430

    New 2to3 cleanups

--- CI/futurize.sh
+++ CI/futurize.sh
@@ -12,7 +12,7 @@ begin=$(date +"%s")
 
 echo ""
 echo "2to3 safe cleanup"
-find . -name "*.py" -type f -exec 2to3 -f raise -f except -f has_key -f idioms -f paren -f ne -f isinstance -w -n {} \;
+find . -name "*.py" -type f -exec 2to3 -f raise -f except -f has_key -f idioms -f paren -f ne -f isinstance -f exec -f apply -f execfile -f xreadlines -f numliterals -w -n {} \;
 find . -name "*.bak" -type f -exec rm -f {} \;
 git add -u
 git add *
